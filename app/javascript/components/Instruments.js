45bf12db6af4ec72aaa5ccc0c9c756d2f1537d6b
jayAardvark <jrcenina@gmail.com>
2021-02-22 00:47:25 -0800

    test first fetch of data and try using hooks

--- app/javascript/components/Instruments.js
+++ app/javascript/components/Instruments.js
@@ -1,10 +1,30 @@
-import React from 'react'
+import React, {Fragment, useState, useEffect } from 'react'
+import axios from 'axios';
+
 
 const Instruments = () => {
+  // try hooks for this
+  const [instruments, setInstruments] = useState([]);
+
+  useEffect(() => {
+    axios.get('/api/v1/instruments')
+    .then(res => {
+      setInstruments(res.data.data)
+    })
+    .catch(res => console.log(res))
+  }, [instruments.length]) // effect will re-run only if instruments changes. revisit docs.
+
+  const currentInstruments = instruments.map(item => {
+    return(
+      <p key={item.attributes.name}>name: {item.attributes.name}</p>
+    )
+  })
+
   return(
-    <p>
-      To do: plan out the flow of these routes
-    </p>
+    <div>
+      <p>To do: plan out the flow of these routes</p>
+      {currentInstruments}
+    </div>
   )
 }
 
